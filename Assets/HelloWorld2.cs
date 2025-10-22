30e049dbec2713fc3eef5c265aac81740e78ecd5
boaepark17-cell <boaepark17@gmail.com>
2025-10-22 15:10:44 +0900

    친구 커밋

--- Assets/HelloWorld2.cs
+++ Assets/HelloWorld2.cs
@@ -7,7 +7,7 @@ public class HelloWorld2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("친구가 수정해서 다시 올림");
     }
 
     // Update is called once per frame
