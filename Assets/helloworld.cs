3cd2bd211ac9a3945d5db239ce14bfbc5b963685
Scarecrow1625 <sag16258@gmail.com>
2025-10-22 16:54:17 +0900

    Update helloworld.cs

--- Assets/helloworld.cs
+++ Assets/helloworld.cs
@@ -7,7 +7,7 @@ public class helloworld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("TEST");
     }
 
     // Update is called once per frame
